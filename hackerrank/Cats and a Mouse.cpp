a94ec80b00569f096a642d8c3b93f66aa6ffa500
CodingIsPain <86591649+trong11@users.noreply.github.com>
2021-07-02 16:54:55 +0700

    Add files via upload

---
+++ hackerrank/Cats and a Mouse.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+		int x,y,z;
+		cin>>x>>y>>z;
+		
+		if(abs(x-z)>abs(y-z)) cout<<"Cat B"<<endl;
+		else if (abs(x-z)<abs(y-z)) cout<<"Cat A"<<endl;
+		else if (abs(x-z)==abs(y-z)) cout<<"Mouse C"<<endl;
+	}
+}
