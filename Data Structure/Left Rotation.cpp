08679ebd1178d5db0e800c50d76e800eecaed1c3
CodingIsPain <86591649+trong11@users.noreply.github.com>
2021-07-18 20:08:41 +0700

    Update Left Rotation.cpp

--- Data Structure/Left Rotation.cpp
+++ Data Structure/Left Rotation.cpp
@@ -5,11 +5,15 @@ int main(){
 	int n,d;
 	cin>>n>>d;
 	int a[n+2];
+	int b[n+2]={0};
 	
 	for(int i=0;i<n;i++) {
-		int z=(i+n-d)%n;
-		cin>>a[z];
+		cin>>a[i];
 	}
 	
-	for(int i=0;i<n;i++) cout<<a[i]<<" ";
+	for(int i=0;i<n;i++){
+		b[i]=a[(i+d)%n];
+	}
+	
+	for(int i=0;i<n;i++) cout<<b[i]<<" ";
 }
