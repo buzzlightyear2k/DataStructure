129227b00e3b95079b9ea931cdf133ff5e58da2c
CodingIsPain <86591649+trong11@users.noreply.github.com>
2021-07-18 20:50:11 +0700

    Update Sequence Equation.cpp

--- hackerrank/Sequence Equation.cpp
+++ hackerrank/Sequence Equation.cpp
@@ -5,13 +5,11 @@ int main(){
 	int n;
 	cin>>n;
 	int p[n+2];
-	int q[n+2]={0};
 	for(int i=1;i<=n;i++) cin>>p[i];
 	
 	for(int i=1;i<=n;i++){
 		for(int j=1;j<=n;j++){
-			if(p[p[j]]==i){
-				q[i]=j;
+			if(p[p[j]]==i){			
 				cout<<j<<endl;
 			}
 		}
